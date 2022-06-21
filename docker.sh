228e0043d50328b3e223ec558b1e06828e9eebcb
Brad Brown <brad@bradsbrown.com>
2022-06-21 18:33:09 -0500

    fix debug in docker cmd

--- docker.sh
+++ docker.sh
@@ -2,9 +2,10 @@
 
 docker build -t wordlinator:latest .
 
-if [ "$1" = "--debug" ]; then
+if [ "$1" == "--debug" ]; then
     shift
-    docker run -d --rm -p 8050:8050 -e DEBUG=true -e DB_PORT -e DB_HOST -e DB_PASS "$@" --name wordlinator wordlinator:latest
+    echo "Running debug mode..."
+    docker run -d --rm -p 8050:8050 -e DASH_DEBUG=true -e DB_PORT -e DB_HOST -e DB_PASS "$@" --name wordlinator wordlinator:latest
 else
     docker run -d --rm -p 8050:8050 -e DB_PORT -e DB_HOST -e DB_PASS "$@" --name wordlinator wordlinator:latest
 fi
